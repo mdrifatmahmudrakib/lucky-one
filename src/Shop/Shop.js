51ba9dd9d6bbc266dc98ae4405bd86e66a654ba4
Rifat Mahmud <mdrifatmahmudrakib@gmail.com>
2022-03-26 06:09:29 +0600

    product name display done

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -3,13 +3,20 @@ import Product from '../Component/Header/Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([])
+
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, []);
 
+    const handleAddToClick = (product) => {
 
+
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
 
 
 
@@ -20,11 +27,19 @@ const Shop = () => {
                     products.map(product => <Product
                         key={product.id}
                         product={product}
+                        handleAddToClick={handleAddToClick}
                     ></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h1>hello</h1>
+                <p>Selected Items: {cart.length}</p>
+
+                {
+                    cart.map(item => <p key={item.id} >{item.name}
+                    </p>
+                    )
+                }
             </div>
         </div>
     );
